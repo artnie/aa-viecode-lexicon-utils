fb7bd89a55f83cfb5a0e696f49f2caf2ac844e11
Arthur Niedzwiecki <arthur.niedzwiecki@gmail.com>
2018-05-16 14:29:49 +0200

    Update lexiconPageUtils.js

--- lexiconPageUtils.js
+++ lexiconPageUtils.js
@@ -2,7 +2,7 @@
 // @name         AA Lexicon Utils
 // @namespace    https://bibliothek.aventurische-allianz.de/entry/*
 // @version      0.1
-// @description  Some util functions for the VieCode lexicon.
+// @description  Some utils functions when traversing the VieCode lexicon
 // @author       Arthur Niedzwiecki
 // @match        https://bibliothek.aventurische-allianz.de/entry/*
 // @grant        none
@@ -12,14 +12,17 @@
   Add listener on left/right key, to get previous/next page.
 */
 document.onkeydown = function(e) {
-    switch (e.keyCode) {
-        case 37:
-            var prev = $('.lexiconPreviousEntryButton a')[0];
-            if (prev) prev.click();
-            break;
-        case 39:
-            var next = $('.lexiconNextEntryButton a')[0];
-            if (next) next.click();
-            break;
+    if (!($('textarea')[0] === $(document.activeElement)[0]))
+    {
+        switch (e.keyCode) {
+            case 37:
+                var prev = $('.lexiconPreviousEntryButton a')[0];
+                if (prev) prev.click();
+                break;
+            case 39:
+                var next = $('.lexiconNextEntryButton a')[0];
+                if (next) next.click();
+                break;
+        }
     }
 };
